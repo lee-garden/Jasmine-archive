35cd42443a810f49e24b4aa7adfc709474c7c7af
gardenLee <khjvvv7@gmail.com>
2021-09-29 20:33:34 +0900

    Hello jasmine

---
+++ src/test/java/lee/garden/jasmine/JasmineApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.garden.jasmine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasmineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
